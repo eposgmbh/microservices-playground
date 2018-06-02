365182927c392103ab5e6a15cb839e132a792c79
Jan Bohlen <jan.bohlen@outlook.de>
2018-06-02 18:19:01 +0200

    Implement centralized logging infrastructure with Graylog (closes #1)

--- src/services/todos/ToDos.WebApi/Program.cs
+++ src/services/todos/ToDos.WebApi/Program.cs
@@ -4,9 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Gelf.Extensions.Logging;
+
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 #pragma warning disable 1591
@@ -23,6 +26,19 @@ namespace ToDos.WebApi
             WebHost
                 .CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0:5000");
+                .UseUrls("http://0.0.0.0:5000")
+                .ConfigureLogging((context, builder) => {
+                    builder.Services.Configure<GelfLoggerOptions>(context.Configuration.GetSection("Graylog"));
+
+                    builder.Services.PostConfigure<GelfLoggerOptions>(options => {
+                        options.AdditionalFields["tag"] = "todos-service";
+                    });
+
+                    builder
+                        .AddConfiguration(context.Configuration.GetSection("Logging"))
+                        .AddConsole()
+                        .AddDebug()
+                        .AddGelf();
+                });
     }
 }
